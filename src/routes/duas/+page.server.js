8f43bb9f3bf8775f7328655bf9ed89b79b40ca07
Muhsin Abboud <abboudmuhsin@gmail.com>
2024-07-15 14:53:50 +0200

    Display on duas overview page at least 100 duas

--- src/routes/duas/+page.server.js
+++ src/routes/duas/+page.server.js
@@ -2,7 +2,7 @@ import { supabase } from '$lib/supabaseClient';
 
 export async function load() {
 
-    const { data, error } = await supabase.functions.invoke('duas?languages=ar,translit,en&page=1&size=10');
+    const { data, error } = await supabase.functions.invoke('duas?languages=ar,translit,en&page=1&size=100');
 	console.log("data: ", data);
 	if (error) {
 		console.error('Error fetching duas:', error);
